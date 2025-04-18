36925b34a518da9b891bc06bf47a23fdeae40828
ozturk50 <ozturk50@ppcloud.nl>
2025-04-18 15:02:07 +0200

    GetAll en Create methodes toegevoegd aan controller

--- src/main/java/org/example/personassignment/controller/PersonController.java
+++ src/main/java/org/example/personassignment/controller/PersonController.java
@@ -1,4 +1,33 @@
 package org.example.personassignment.controller;
 
+import jakarta.validation.Valid;
+import org.example.personassignment.service.PersonService;
+import org.example.personassignment.dto.PersonRequestDTO;
+import org.example.personassignment.dto.PersonResponseDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/persons")
 public class PersonController {
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<PersonResponseDTO>> getAllPersons() {
+        List<PersonResponseDTO> persons = personService.getAllPersons();
+        return ResponseEntity.ok(persons);
+    }
+
+    @PostMapping
+    public ResponseEntity<PersonResponseDTO> createPerson(@Valid @RequestBody PersonRequestDTO personRequestDTO) {
+        PersonResponseDTO responseDTO = personService.createPerson(personRequestDTO);
+        return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
+    }
 }
