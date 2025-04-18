4d51c936b91c47d147a6b0ea60957ad990be3f98
ozturk50 <ozturk50@ppcloud.nl>
2025-04-18 15:05:06 +0200

    Person Mapper voor toEntity en toDto

--- src/main/java/org/example/personassignment/mapper/PersonMapper.java
+++ src/main/java/org/example/personassignment/mapper/PersonMapper.java
@@ -1,25 +1,42 @@
 package org.example.personassignment.mapper;
 
+import org.example.personassignment.dto.PersonRequestDTO;
 import org.example.personassignment.dto.PersonResponseDTO;
 import org.example.personassignment.entity.Person;
+import org.springframework.stereotype.Component;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
+@Component
 public class PersonMapper {
-    public PersonResponseDTO toDto(Person person) {
-        List<Long> childrenIds = person.getChildren().stream()
-                .map(Person::getId)
-                .collect(Collectors.toList());
+    public Person toEntity(PersonRequestDTO personRequestDTO) {
+        Person parent1 = personRepository.findById(personRequestDTO.parent1Id())
+                .orElseThrow(() -> new RuntimeException("Parent 1 not found"));
+        Person parent2 = personRepository.findById(personRequestDTO.parent2Id())
+                .orElseThrow(() -> new RuntimeException("Parent 2 not found"));
+        Person partner = personRequestDTO.partnerId() != null
+                ? personRepository.findById(personRequestDTO.partnerId())
+                .orElseThrow(() -> new RuntimeException("Partner not found"))
+                : null;
 
+        Person person = new Person();
+        person.setName(personRequestDTO.name());
+        person.setBirthDate(personRequestDTO.birthDate());
+        person.setParent1(parent1);
+        person.setParent2(parent2);
+        person.setPartner(partner);
+        return person;
+    }
+
+    public PersonResponseDTO toDto(Person person) {
         return new PersonResponseDTO(
                 person.getId(),
                 person.getName(),
                 person.getBirthDate(),
-                person.getParent1() != null ? person.getParent1().getId() : null,
-                person.getParent2() != null ? person.getParent2().getId() : null,
+                person.getParent1().getId(),
+                person.getParent2().getId(),
                 person.getPartner() != null ? person.getPartner().getId() : null,
-                childrenIds
+                person.getChildren().stream().map(Person::getId).collect(Collectors.toList())
         );
     }
 
