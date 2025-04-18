0df73624b19b835ab124a3e3f0dba8c22b1b0a09
ozturk50 <ozturk50@ppcloud.nl>
2025-04-18 13:45:28 +0200

    Project opstart

---
+++ src/main/java/org/example/personassignment/PersonAssignmentApplication.java
@@ -0,0 +1,13 @@
+package org.example.personassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/personassignment/PersonAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.personassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
