22c0be7a67525ae3574593e1f95ef1f1015a35ea
ozturk50 <ozturk50@ppcloud.nl>
2025-04-18 15:22:26 +0200

    Local database aangemaakt PersonRepository extend een JpaRepository

--- src/main/java/org/example/personassignment/repository/PersonRepository.java
+++ src/main/java/org/example/personassignment/repository/PersonRepository.java
@@ -1,4 +1,10 @@
 package org.example.personassignment.repository;
 
-public class PersonRepository {
+import org.example.personassignment.entity.Person;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Long> {
+
 }
